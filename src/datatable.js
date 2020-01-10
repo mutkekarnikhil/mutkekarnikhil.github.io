ebf8027e66c67c14a0c2bd15660e12ed1efd282f
Nikhil Mutkekar <Nikhil.Mutkekar@in.unisys.com>
2020-01-10 23:53:20 +0530

    Added functionality to react components - Fetch data from API and Display in table without lazy loading

--- src/datatable.js
+++ src/datatable.js
@@ -5,8 +5,24 @@ export default class DataTable extends React.Component {
     render() {
         return (
             <div className="data-table">
-                <input className="filter-input" type="text" placeholder="Search Title" />
-                <Table />
+                <div className="filter-container">
+                    <select className="select-filter" onChange={(e) => this.props.onFilterColumnSelect(e.target)}>
+                        <option value="" disabled selected label="Filter"></option>
+                        <option value="title" label="Title"></option>
+                        <option value="url" label="URL"></option>
+                        <option value="albumId" label="Album ID"></option>
+                    </select>
+                    <input className="input-filter" type="text" placeholder={`Search ${this.props.filterColumn}`} onKeyUp={(e) => this.props.filterRowsBasedOnTitle(e.target.value)} />
+                </div>
+                {this.props.rows.length ? (
+                    <Table
+                        columns={this.props.columns}
+                        rows={this.props.rows}
+                        onRowClick={this.props.onRowClick}
+                    />
+                ) : (
+                    <div className="no-records"> No records found </div>
+                )}
             </div>
         )
     }
@@ -18,27 +34,21 @@ class Table extends React.Component {
             <table className="table-content">
                 <thead>
                     <tr className="table-row">
-                        <th className="th-thumbnail"></th>
-                        <th className="th-title">Title</th>
-                        <th className="th-url">URL</th>
+                        {this.props.columns.map(column => (
+                            <th className={`th-${column.id}`} align={column.numeric ? 'right' : 'left'}>{column.label}</th>
+                        ))}
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="table-row">
-                        <td className="td-thumbnail"></td>
-                        <td className="td-title">React</td>
-                        <td className="td-url">1.0</td>
-                    </tr>
-                    <tr className="table-row">
-                        <td className="td-thumbnail"></td>
-                        <td className="td-title">Angular</td>
-                        <td className="td-url">178.0</td>
-                    </tr>
-                    <tr className="table-row">
-                        <td className="td-thumbnail"></td>
-                        <td className="td-title">React-Native</td>
-                        <td className="td-url">109.0</td>
-                    </tr>
+                    {this.props.rows.map((row, i) => (
+                        <tr className="table-row">
+                            <td><input className="td-select-checkbox" type="checkbox" onClick={() => this.props.onRowClick(row, i)} /></td>
+                            <td className="td-thumbnail"><img src={row.thumbnailUrl} alt="img" width="30px" height="30px" /></td>
+                            <td className="td-title">{row.title}</td>
+                            <td className="td-url">{row.url}</td>
+                            <td className="td-albumId">{row.albumId}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         )
--- src/index.js
+++ src/index.js
@@ -4,9 +4,97 @@ import ReactDOM from 'react-dom'
 import DataTable from './datatable'
 
 export default class App extends React.Component {
-    render(){
-        return <DataTable />
+    constructor() {
+        super()
+        this.state = {
+            rowData: [],
+            filterColumn: {label: 'Title', value: 'title'}
+        }
+        this.allData = []
+        this.filterRowsBasedOnTitle = this.filterRowsBasedOnTitle.bind(this)
+        this.onFilterColumnSelect = this.onFilterColumnSelect.bind(this)
+    }
+
+    componentWillMount() {
+        fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(response => {
+            if (response.status !== 200) {
+                console.log(`Oops! There was problem fetching data from API. Status Code: ${response.status}`)
+                return null
+            }
+            return response.json()
+        })
+        .then((responseData) => {
+            if (!responseData) return
+            this.allData = responseData.slice(0,100)
+            this.setState({ rowData: this.allData })
+        })
+    }
+
+    getColumnObject() {
+        return [{
+            id: 'select-checkbox',
+            label: '',
+            numeric: false
+        },{
+            id: 'thumbnail',
+            label: '',
+            numeric: false,
+        }, {
+            id: 'title',
+            label: 'Title',
+            numeric: false
+        }, {
+            id: 'url',
+            label: 'URL',
+            numeric: false
+        }, {
+            id: 'albumId',
+            label: 'Album ID',
+            numeric: true
+        }]
+    }
+
+    filterRowsBasedOnTitle(value) {
+        this.filteredText = value
+        if(this.filteredText) {
+            this.setState({
+                rowData: this.allData.filter(row => row[this.state.filterColumn.value].toString().includes(this.filteredText))
+            })
+        } else {
+            this.setState({
+                rowData: this.allData
+            })
+        }
+    }
+
+    onFilterColumnSelect(selectedColumn) {
+        this.setState({ 
+            filterColumn: {
+                value: selectedColumn.value,
+                label: selectedColumn.selectedOptions[0].label
+            }
+        }, () => {
+            this.filterRowsBasedOnTitle(this.filteredText)
+        })
+    }
+
+    onRowClick(rowData, rowIndex) {
+        console.log(rowData, rowIndex)
+    }
+
+    render() {
+        return (
+            <DataTable
+                columns={this.getColumnObject()}
+                rows={this.state.rowData}
+                filterRowsBasedOnTitle={this.filterRowsBasedOnTitle}
+                onRowClick={this.onRowClick}
+                filterColumn={this.state.filterColumn.label}
+                onFilterColumnSelect={this.onFilterColumnSelect}
+            />
+        )
     }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector('#root'))
